773c6eb4e37ec507f9465851e27da51fb44ae599
remove158 <remove158@gmail.com>
2025-01-03 23:51:22 +0700

    fix: env

--- apps/player/docker-entrypoint.sh
+++ apps/player/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Replace environment variables in the template
-envsubst < /opt/Lavalink/application.yml.template > /opt/Lavalink/application.yml
+envsubst < application.yml.template > /opt/Lavalink/application.yml
 
 # Start Lavalink
 exec java -Djdk.tls.client.protocols=TLSv1.2 -jar Lavalink.jar
